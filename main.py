f837910386f681a8bd57267a6084944a38c9727a
Romāns Vēveris 221RDB094 <125798504+RomansVeveris@users.noreply.github.com>
2023-02-19 18:17:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,31 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+        	# Process opening bracket, write your code here
+        	opening_brackets_stack.append(Bracket(next, i+1))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+        	# Process closing bracket, write your code here
+        	# print(opening_brackets_stack)
+        	if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+        		return i + 1
+        	opening_brackets_stack.pop()
+
+    if opening_brackets_stack:
+    	return opening_brackets_stack[0].position
+
+
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+
+    if not mismatch:
+    	print("Success")
+    else:
+    	print(mismatch)
 
 
 if __name__ == "__main__":
